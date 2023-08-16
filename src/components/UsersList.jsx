5a98afd361d89092dfb11494a418e8d2496368a2
Mohammed Usama <usamabhakrani@gmail.com>
2023-08-16 15:44:06 +0500

    setting up useeffect for fetching data on pageload

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,4 +1,13 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchUsers } from "../store";
+
 const UsersList = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
   return "Users List";
 };
 
