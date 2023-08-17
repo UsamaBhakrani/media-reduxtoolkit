5e17cb75f2b4f50f5064912c8f1064c288363f7e
Mohammed Usama <usamabhakrani@gmail.com>
2023-08-17 14:15:27 +0500

    Defining Component state with useState

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,27 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers, addUsers } from "../store";
 import Skeleton from "./Skeleton";
 import Button from "./Button";
 
 const UsersList = () => {
+  const [isLoadingUsers, setIsLoadingUsers] = useState(false);
+  const [loadingUsersError, setLoadingUsersError] = useState(null);
+
   const dispatch = useDispatch();
-  const { data, isLoading, error } = useSelector((state) => {
+  const { data } = useSelector((state) => {
     return state.users;
   });
 
   useEffect(() => {
-    dispatch(fetchUsers());
+    setIsLoadingUsers(true);
+    dispatch(fetchUsers())
+      .unwrap()
+      .catch((error) => setLoadingUsersError(error))
+      .finally(() => setIsLoadingUsers(false));
   }, [dispatch]);
 
   const handleUserAdd = () => {
     dispatch(addUsers());
   };
 
-  if (isLoading) {
+  if (isLoadingUsers) {
     return <Skeleton times={5} className="h-10 w-full" />;
   }
-  if (error) {
+  if (loadingUsersError) {
     return <div>Error Fetching Data...</div>;
   }
 
