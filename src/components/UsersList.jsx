f984e9e4590381e8db694696286fdde28d69b2e2
Mohammed Usama <usamabhakrani@gmail.com>
2023-08-18 16:56:11 +0500

    UsersList Component to simplify code

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -4,6 +4,7 @@ import { fetchUsers, addUsers, deleteUsers } from "../store";
 import Skeleton from "./Skeleton";
 import Button from "./Button";
 import { useThunk } from "../hooks/use-thunk";
+import UsersListItem from "./UsersListItem";
 
 const UsersList = () => {
   const [doFetchUsers, isLoadingUsers, loadingUsersError] =
@@ -30,13 +31,8 @@ const UsersList = () => {
     content = <div>Error Fetching Data...</div>;
   } else {
     content = data.map((user) => {
-      return (
-        <div key={user.id} className="mb-2 border rounded">
-          <div className="flex p-2 items-center justify-between items-center cursor-pointer">
-            {user.name}
-          </div>
-        </div>
-      );
+      return <UsersListItem key={user.id} user={user}/>
+      
     });
   }
 
--- src/store/thunks/deleteUsers.jsx
+++ src/store/thunks/deleteUsers.jsx
@@ -1,9 +1,9 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const deleteUsers = createAsyncThunk("delete/user", async (userId) => {
-  const request = await axios.delete(`http://localhost:5174/users/${userId}`);
-  return;
+const deleteUsers = createAsyncThunk("user/delete", async (user) => {
+  const request = await axios.delete(`http://localhost:5174/users/${user.id}`);
+  return request.data
 });
 
 export { deleteUsers };
